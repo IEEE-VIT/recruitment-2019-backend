1ef00ad645b884e78ead5e6eaae7c3bdb7b217ab
aryan <aryan@Aryans-MacBook-Pro.local>
2019-11-06 09:34:15 +0530

    Full functionality for candidates implemented. Fixed bug in previous commit: Answers corresponding to a candidate now appear in the response when retrieving a candidate.

--- candidate/serializers.py
+++ candidate/serializers.py
@@ -2,29 +2,21 @@ from rest_framework import serializers
 
 from candidate.models import Answer, Candidate
 
+from drf_writable_nested import WritableNestedModelSerializer
 
-class AnswerSerializer(serializers.HyperlinkedModelSerializer):
+
+class AnswerSerializer(serializers.ModelSerializer):
 
 	class Meta:
 		model = Answer
-		fields = ['url', 'id', 'answer']
+		fields = ['id', 'answer']
 
 
-class CandidateSerializer(serializers.HyperlinkedModelSerializer):
-	 
-	answers = AnswerSerializer(many=True, required=False)
+class CandidateSerializer(WritableNestedModelSerializer):
+
+	answers = AnswerSerializer(many=True, source='candidate_answers')
 
 	class Meta:
 		model = Candidate
-		fields = ['url', 'id', 'name', 'contact', 'reg_no', 'email', 'hostel', 'grade', 'comment', 'answers']
+		fields = ['id', 'name', 'contact', 'reg_no', 'email', 'hostel', 'grade', 'comment', 'answers']
 
-	def create(self, validated_data):
-		print(validated_data)
-		if 'answers' in validated_data.keys():
-			answers_data = validated_data.pop('answers')
-			candidate = Candidate.objects.create(**validated_data)
-			for answer_data in answers_data:
-				Answer.objects.create(candidate_id=candidate, **answer_data)
-			return candidate
-		else:
-			return Candidate(**validated_data)
