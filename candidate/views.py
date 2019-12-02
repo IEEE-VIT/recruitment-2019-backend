42eedfe508a403b185d976bea14957565bc379a4
SameeranB <sameeranbandishti93@gmail.com>
2019-12-02 15:22:47 +0530

    Superuser Permission

--- candidate/views.py
+++ candidate/views.py
@@ -1,9 +1,11 @@
 from rest_framework.viewsets import ModelViewSet
 
+from Recruitement_Website_Backend.permissions import IsSuperuser
 from candidate.models import Candidate
 from candidate.serializers import CandidateSerializer
 
 
 class CandidateViewSet(ModelViewSet):
+	permission_classes = [IsSuperuser]
 	serializer_class = CandidateSerializer
 	queryset = Candidate.objects.all()
