7ce71ef042afab32e409ca0e78a00c9bc98b76f8
SameeranB <sameeranbandishti93@gmail.com>
2019-12-03 13:20:16 +0530

    custom create method

--- rsvp/serializers.py
+++ rsvp/serializers.py
@@ -10,3 +10,8 @@ class RSVPSerializer(serializers.ModelSerializer):
     class Meta:
         model = RSVP
         exclude = ['timestamp', 'id']
+
+    def create(self, validated_data):
+        rsvp = RSVP.objects.create(name=validated_data['name'], contact=validated_data['contact'], reg_no=validated_data['reg_no'], email=validated_data['email'])
+        return rsvp
+
