d3a310fd7471bece114fa52e6a2307a0ce300868
SameeranB <sameeranbandishti93@gmail.com>
2019-12-04 01:43:50 +0530

    Specializing Candidate Viewsets

--- candidate/views.py
+++ candidate/views.py
@@ -1,48 +1,50 @@
 import django_filters
-from rest_framework import viewsets
-from rest_framework.mixins import ListModelMixin
+from requests import Response
+from rest_framework import viewsets, status
+from rest_framework.decorators import action
+from rest_framework.mixins import ListModelMixin, CreateModelMixin, UpdateModelMixin, RetrieveModelMixin
+from rest_framework.permissions import AllowAny, IsAuthenticated, IsAdminUser
 
 from candidate.models import Candidate
 from candidate.serializers import CandidateSerializer
 
 
-# class CandidateViewSet(ModelViewSet):
-# 	queryset = Candidate.objects.all()
-# 	serializer_class = CandidateSerializer
-#
-# 	def get_permissions(self):
-# 		permission_classes = []
-# 		if self.action == 'create':
-# 			permission_classes = [AllowAny]
-# 		elif self.action == 'partial_update' or self.action == 'list' or self.action == 'retrieve':
-# 			permission_classes = [IsAuthenticated]
-# 		else:
-# 			permission_classes = [IsAdminUser]
-# 		return [permission() for permission in permission_classes]
+class CandidateViewSet(viewsets.GenericViewSet, CreateModelMixin, UpdateModelMixin, RetrieveModelMixin):
+    queryset = Candidate.objects.all()
+    serializer_class = CandidateSerializer
+    lookup_field = 'id'
+    lookup_url_kwarg = 'candidate_id'
 
-#
-# @api_view(['PATCH'])
-# @permission_classes(IsAuthenticated)
-# def send_email_to_candidate(request, pk):
-# 	try:
-# 		candidate = Candidate.objects.get(id=pk)
-# 	except Candidate.DoesNotExist:
-# 		return Response(status=status.HTTP_404_NOT_FOUND)
-#
-# 	if request.method == 'PATCH':
-# 		try:
-# 			subject = 'IEEE-VIT 2019 Recruitments'
-# 			message = f"Hi, {candidate.name}! Please report to {request.user.recruiter_set.all()}"
-# 			print(message)
-# 			send_mail(
-# 				subject=subject,
-# 				message=message,
-# 				from_email='jaiswalsanskar078@gmail.com',
-# 				recipient_list= [candidate.email]
-# 			)
-# 			candidate.email_sent = True
-# 		except:
-# 			print('Couldn\'t send email to candidate')
+    def get_permissions(self):
+        if self.action == 'create':
+            permission_classes = [AllowAny]
+        elif self.action == 'partial_update' or self.action == 'retrieve':
+            permission_classes = [IsAuthenticated]
+        else:
+            permission_classes = [IsAdminUser]
+        return [permission() for permission in permission_classes]
+
+    @action(methods=['POST'], detail=True)
+    def snooze(self, request, *args, **kwargs):
+        try:
+            candidate = Candidate.objects.get(id=kwargs['candidate_id'])
+        except Candidate.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+        if request.method == 'PATCH':
+            try:
+                subject = 'IEEE-VIT 2019 Recruitments'
+                message = f"Hi, {candidate.name}! Please report to {request.user.recruiter_set.all()}"
+                print(message)
+                send_mail(
+                    subject=subject,
+                    message=message,
+                    from_email='jaiswalsanskar078@gmail.com',
+                    recipient_list=[candidate.email]
+                )
+                candidate.email_sent = True
+            except:
+                print('Couldn\'t send email to candidate')
 
 
 class CandidateListViewset(viewsets.GenericViewSet, ListModelMixin):
@@ -50,3 +52,4 @@ class CandidateListViewset(viewsets.GenericViewSet, ListModelMixin):
     filter_backends = [django_filters.rest_framework.DjangoFilterBackend]
     filterset_fields = ['room_number', 'interests']
     serializer_class = CandidateSerializer
+
