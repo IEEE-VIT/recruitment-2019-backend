b062248d81f74a9a8d7b5947a89751721a0bdb25
SameeranB <sameeranbandishti93@gmail.com>
2019-12-03 12:57:45 +0530

    propagate

--- Recruitement_Website_Backend/settings.py
+++ Recruitement_Website_Backend/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-DEBUG_PROPAGATE_EXCEPTIONS = False
+DEBUG_PROPAGATE_EXCEPTIONS = True
 
 ALLOWED_HOSTS = ['*']
 
