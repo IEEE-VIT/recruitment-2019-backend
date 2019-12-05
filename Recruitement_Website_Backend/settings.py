e9aace2b6c9f1801d0a41a4e3bd7355215d4eaac
SameeranB <sameeranbandishti93@gmail.com>
2019-12-05 19:18:58 +0530

    Removed XFrame middleware

--- Recruitement_Website_Backend/settings.py
+++ Recruitement_Website_Backend/settings.py
@@ -96,7 +96,6 @@ MIDDLEWARE = [
 	'django.middleware.csrf.CsrfViewMiddleware',
 	'django.contrib.auth.middleware.AuthenticationMiddleware',
 	'django.contrib.messages.middleware.MessageMiddleware',
-	'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
 CORS_ORIGIN_ALLOW_ALL = True
