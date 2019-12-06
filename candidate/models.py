723d104a0b127da3cb00870ccf0ea45805f92734
SameeranB <sameeranbandishti93@gmail.com>
2019-12-06 17:24:56 +0530

    Blank True

--- candidate/models.py
+++ candidate/models.py
@@ -26,14 +26,14 @@ class Candidate(models.Model):
     room_number = models.CharField(max_length=10)
 
     round_1_comment = models.TextField(blank=True)
-    round_1_call = models.BooleanField(default=None, null=True)
+    round_1_call = models.BooleanField(default=None, null=True, blank=True)
 
-    round_2_project_template = models.ForeignKey(ProjectTemplate, on_delete=models.PROTECT, default=None, null=True)
+    round_2_project_template = models.ForeignKey(ProjectTemplate, on_delete=models.PROTECT, default=None, null=True, blank=True)
     round_2_project_modification = models.TextField(default=None, blank=True, null=True)
-    round_2_comment = models.TextField(default=None, null=True)
-    round_2_project_completion = models.IntegerField(default=0, null=True)
-    round_2_project_understanding = models.IntegerField(default=0, null=True)
-    round_2_call = models.BooleanField(default=False, null=True)
+    round_2_comment = models.TextField(default=None, null=True, blank=True)
+    round_2_project_completion = models.IntegerField(default=0, null=True, blank=True)
+    round_2_project_understanding = models.IntegerField(default=0, null=True, blank=True)
+    round_2_call = models.BooleanField(default=False, null=True, blank=True)
 
     called_by = models.ForeignKey(User, on_delete=models.PROTECT, related_name="interviewee", null=True, blank=True)
 
