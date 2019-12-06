3bd182f07d2d44baf48ed622e2b881ea514121df
Harjyot Bagga <imharjyotbagga@gmail.com>
2019-12-06 14:31:27 +0530

    Admin panel altered

--- recruiter/admin.py
+++ recruiter/admin.py
@@ -2,4 +2,9 @@ from django.contrib import admin
 
 from .models import User
 
-admin.site.register(User)
+# admin.site.register(User)
+@admin.register(User)
+class CandidateAdmin(admin.ModelAdmin):
+    list_display = ('username', 'room_no')
+    list_filter = ('username', 'room_no')
+    search_fields = ('username', 'room_no')
