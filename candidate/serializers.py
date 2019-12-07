046d060500200a91e6a927b70865debf39b30059
aryan <aryan@Aryans-MacBook-Pro.local>
2019-12-07 14:00:09 +0530

    Adding a recaptcha field

--- candidate/serializers.py
+++ candidate/serializers.py
@@ -21,17 +21,17 @@ class CandidateInterviewerSerializer(serializers.ModelSerializer):
 
 
 class CandidateSerializer(WritableNestedModelSerializer):
-    answers = AnswerSerializer(many=True, source='candidate_answers')
+    answers = AnswerSerializer(many=True, source='candidate_answers', required=False)
     recaptcha_field = ReCaptchaField()
     called_to = serializers.SerializerMethodField()
 
     class Meta:
         model = Candidate
-        fields = '__all__'
+        fields = ['id', 'name', 'contact', 'email', 'hostel', 'reg_no', 'interests', 'answers', 'question1_text', 'question2_text', 'question3_text', 'room_number', 'called_to']
         read_only_fields = ['id', 'is_active', 'times_snoozed', 'called', 'timestamp', 'round_1_comment',
                             'round_1_call', 'round_2_project_template', 'round_2_project_modification',
                             'round_2_comment', 'round_2_project_completion', 'round_2_project_understanding',
-                            'round_2_call', 'called_by', 'called_to_room_no', 'interviewer_switched']
+                            'round_2_call', 'called_by', 'called_to_room_no', 'interviewer_switched', 'called_to']
 
     def get_called_to(self, instance):
         if instance.called:
@@ -40,6 +40,7 @@ class CandidateSerializer(WritableNestedModelSerializer):
             return None
 
     def create(self, validated_data):
+        print(validated_data)
         validated_data.pop('recaptcha_field')
         return super().create(**validated_data)
 
