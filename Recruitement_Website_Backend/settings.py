8ebe57eb740fac528a1cd157018bacdd80741e97
Sameeran Bandishti <35862963+SameeranB@users.noreply.github.com>
2019-12-27 09:15:52 +0530

    Update settings.py

--- Recruitement_Website_Backend/settings.py
+++ Recruitement_Website_Backend/settings.py
@@ -118,8 +118,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'sambanmicro1203@gmail.com'
-EMAIL_HOST_PASSWORD = 'Sameeran1203'
+
 
 ROOT_URLCONF = 'Recruitement_Website_Backend.urls'
 
